a09af4905d486d00fae68021221800c044721471
吴俊城 <wujunchengman@163.com>
2022-10-10 11:00:21 +0800

    完善表格单元格构造器

--- src/ElementBuilder/TableCellBuilder.cs
+++ src/ElementBuilder/TableCellBuilder.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml.Wordprocessing;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,5 +12,65 @@ namespace KiteDoc.ElementBuilder
     /// </summary>
     public class TableCellBuilder
     {
+        private TableCell tableCell = new();
+        private TableCellProperties tableCellProperties = new TableCellProperties();
+        private TableCellWidth? tableCellWidth;
+        private TableVerticalAlignmentValues tableVerticalAlignmentValues = TableVerticalAlignmentValues.Center;
+
+        /// <summary>
+        /// 设置单元格宽度
+        /// </summary>
+        /// <param name="tableCellWidth">单元格宽度</param>
+        /// <returns></returns>
+        public TableCellBuilder SetTableCellWidth(TableCellWidth tableCellWidth)
+        {
+            this.tableCellWidth = tableCellWidth;
+            return this;
+        }
+
+        public TableCell Build()
+        {
+            // 设置宽度
+            if (tableCellWidth!=null)
+            {
+                var width =  new DocumentFormat.OpenXml.Wordprocessing.TableCellWidth();
+                switch (tableCellWidth.TableWidthType)
+                {
+                    case TableWidthType.Percent:
+                        width.Type = TableWidthUnitValues.Pct;
+                        width.Width = (Math.Round(tableCellWidth.Width, 1) * 50).ToString();
+                        break;
+                    case TableWidthType.Cm:
+                        // Dxa是二十分之一点，其与厘米的换算需要先将cm转换为像素，然后乘以20，Word使用72dpi显示
+
+                        // Dxa =  cm/2.54*72*20
+
+                        // Word中的行为是先计算到像素点，然后保留一位小数，再乘以20，得到二十分之一点值
+                        width.Type = TableWidthUnitValues.Dxa;
+                        width.Width = (Math.Round((tableCellWidth.Width * 72 / 2.54), 1) * 20).ToString();
+                        break;
+                    case TableWidthType.Nil:
+                        width.Type = TableWidthUnitValues.Nil;
+                        break;
+                    case TableWidthType.Auto:
+                        width.Type = TableWidthUnitValues.Auto;
+                        break;
+                    default:
+                        break;
+                }
+                tableCellProperties.TableCellWidth = width;
+            }
+
+            // 设置居中
+            tableCellProperties.TableCellVerticalAlignment = new TableCellVerticalAlignment { Val = tableVerticalAlignmentValues };
+            
+
+            // 绑定属性
+            tableCell.TableCellProperties = tableCellProperties;
+
+            // 返回表格
+            return tableCell;
+        }
+
     }
 }
