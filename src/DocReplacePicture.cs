8401e15a07fbe9e8a7404fc8927f2bfbe5f0339f
吴俊城 <wujunchngman@163.com>
2024-06-27 16:43:41 +0800

    增加获取图片Run扩展方法，以支撑提供替换为多张图片的方法

--- src/DocReplacePicture.cs
+++ src/DocReplacePicture.cs
@@ -2,14 +2,7 @@
 using DocumentFormat.OpenXml.Packaging;
 using KiteDoc.Enum;
 using KiteDoc.Utils;
-using System;
-using System.IO;
 using System.Linq;
-using SixLabors.ImageSharp;
-
-using A = DocumentFormat.OpenXml.Drawing;
-using DW = DocumentFormat.OpenXml.Drawing.Wordprocessing;
-using PIC = DocumentFormat.OpenXml.Drawing.Pictures;
 using DocumentFormat.OpenXml.Wordprocessing;
 
 namespace KiteDoc
@@ -28,130 +21,7 @@ namespace KiteDoc
 
             if (waitReplace.Any())
             {
-                if (!File.Exists(fileName))
-                {
-                    throw new Exception($"未在该路径({fileName})找到图片");
-                }
-                MainDocumentPart mainPart = doc.MainDocumentPart;
-                var imagePartType = ImagePartType.Png;
-                switch (imageType)
-                {
-                    case ImageType.Png:
-                        imagePartType = ImagePartType.Png;
-                        break;
-
-                    case ImageType.Jpeg:
-                        imagePartType = ImagePartType.Jpeg;
-                        break;
-
-                    case ImageType.Bmp:
-                        imagePartType = ImagePartType.Bmp;
-                        break;
-
-                    case ImageType.Gif:
-                        imagePartType = ImagePartType.Gif;
-                        break;
-
-                    case ImageType.Icon:
-                        imagePartType = ImagePartType.Icon;
-                        break;
-                }
-
-                ImagePart imagePart = mainPart.AddImagePart(imagePartType);
-
-                double rate = default;
-                // 将图片写入Word
-                using (FileStream stream = new FileStream(fileName, FileMode.Open))
-                {
-                    imagePart.FeedData(stream);
-                }
-
-                // 再次读取计算宽高比
-                // 这里因为流写过一次就变成空了，需要深入了解后再优化
-                using (FileStream stream = new FileStream(fileName, FileMode.Open))
-                {
-                    var imageInfo = Image.Identify(stream);
-                    rate = (double)imageInfo.Width / (double)imageInfo.Height;
-                }
-
-                string relationshipId = mainPart.GetIdOfPart(imagePart);
-
-                long cx = (long)(360000L * width); //360000L = 1厘米
-                long cy;
-                if (height == -1)
-                {
-                    cy = (long)(360000L * width / rate);
-                }
-                else
-                {
-                    cy = (long)(360000L * height);
-                }
-
-                // Define the reference of the image.
-                var element =
-                     new Drawing(
-                         new DW.Inline(
-                             new DW.Extent() { Cx = cx, Cy = cy },
-                             new DW.EffectExtent()
-                             {
-                                 LeftEdge = 0L,
-                                 TopEdge = 0L,
-                                 RightEdge = 0L,
-                                 BottomEdge = 0L
-                             },
-                             new DW.DocProperties()
-                             {
-                                 Id = (UInt32Value)1U,
-                                 Name = Path.GetFileNameWithoutExtension(fileName)
-                             },
-                             new DW.NonVisualGraphicFrameDrawingProperties(
-                                 new A.GraphicFrameLocks() { NoChangeAspect = true }),
-                             new A.Graphic(
-                                 new A.GraphicData(
-                                     new PIC.Picture(
-                                         new PIC.NonVisualPictureProperties(
-                                             new PIC.NonVisualDrawingProperties()
-                                             {
-                                                 Id = (UInt32Value)0U,
-                                                 Name = "New Bitmap Image.jpg"
-                                             },
-                                             new PIC.NonVisualPictureDrawingProperties()),
-                                         new PIC.BlipFill(
-                                             new A.Blip(
-                                                 new A.BlipExtensionList(
-                                                     new A.BlipExtension()
-                                                     {
-                                                         Uri =
-                                                            "{28A0092B-C50C-407E-A947-70E740481C1C}"
-                                                     })
-                                             )
-                                             {
-                                                 Embed = relationshipId,
-                                                 CompressionState =
-                                                 A.BlipCompressionValues.Print
-                                             },
-                                             new A.Stretch(
-                                                 new A.FillRectangle())),
-                                         new PIC.ShapeProperties(
-                                             new A.Transform2D(
-                                                 new A.Offset() { X = 0L, Y = 0L },
-                                                 new A.Extents() { Cx = cx, Cy = cy }),
-                                             new A.PresetGeometry(
-                                                 new A.AdjustValueList()
-                                             )
-                                             { Preset = A.ShapeTypeValues.Rectangle }))
-                                 )
-                                 { Uri = "http://schemas.openxmlformats.org/drawingml/2006/picture" })
-                         )
-                         {
-                             DistanceFromTop = (UInt32Value)0U,
-                             DistanceFromBottom = (UInt32Value)0U,
-                             DistanceFromLeft = (UInt32Value)0U,
-                             DistanceFromRight = (UInt32Value)0U,
-                             EditId = "50D07946"
-                         });
-
-                var run = new Run(element);
+                var run = doc.GetPictureRun(fileName, imageType, width, height);
 
                 foreach (var item in waitReplace)
                 {
---
+++ test/KiteDocTest/PictureTest.cs
@@ -0,0 +1,40 @@
+﻿using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using KiteDoc;
+using KiteDocTest.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace KiteDocTest
+{
+    public class PictureTest
+    {
+        [Fact]
+        public void ReplaceMultiplePictureTest()
+        {
+            var filename = "替换文本为图片.docx";
+            string testPath = FileUtils.CopyTestFile(filename, "替换多张图片.docx");
+
+            using (WordprocessingDocument doc = WordprocessingDocument.Open(testPath, true))
+            {
+                var files = System.IO.Directory.GetFiles("C:\\VerificationSystemTestStaticFile\\Pictures");
+
+                var runs = files.Take(6).Select(x => doc.GetPictureRun(x, KiteDoc.Enum.ImageType.Png, 8.85));
+
+                var p = new Paragraph();
+                foreach (var item in runs)
+                {
+                    p.AppendChild(item);
+                }
+
+                doc.Replace("假装", new List<Paragraph>() { p });
+                //var count = doc.Replace("假装", "D:/CNAS.png", ImageType.Png);
+                //Assert.Equal(2, count);
+            }
+        }
+    }
+}
